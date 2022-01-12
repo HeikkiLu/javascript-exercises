8a60d8bca86373cbaea1105754673515ab9493b8
HeikkiLu <heikki.luukkonen@gmail.com>
2022-01-12 22:02:49 +0200

    calculator done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  const reducer = (previousValue, currentValue) => previousValue + currentValue;
+  if (arr.length > 1) {
+    return arr.reduce(reducer);
+  } else {
+    if (arr.length != 0) {
+      return arr[0];
+    } else {
+      return 0;
+    }
+  }
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  const reducer = (previousValue, currentValue) => previousValue * currentValue;
+  if (arr.length > 1) {
+    return arr.reduce(reducer);
+  } else {
+    if (arr.length != 0) {
+      return arr[0];
+    } else {
+      return 0;
+    }
+  }
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num == 0) return 1;
+  return num * factorial(num - 1);
 };
 
 // Do not edit below this line
@@ -29,5 +48,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
